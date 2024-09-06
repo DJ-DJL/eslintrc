6198f496e730ac5266cda587119175a36a30f71f
lee <lee@dj-djl.com>
2024-09-06 17:52:18 +0200

    Setting up rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,35 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 import eslintComments from 'eslint-plugin-eslint-comments';
+import sonar from 'eslint-plugin-sonarjs';
 
 
-export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
+export const general = {
+    "settings": {
+        "react": {
+            "version": "0.0" // Explicitly set to avoid React settings if not using React
+        }
+    },
     plugins: {
-      "eslint-comments": eslintComments
+        "eslint-comments": eslintComments,
+        ...sonar.configs.recommended.plugins
     },
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "eslint-comments/require-description": "error",
+        ...sonar.configs.recommended.rules,
+        "eslint-comments/require-description": "error",
+        "sonarjs/cognitive-complexity": ["error", 10],
+        "sonarjs/todo-tag": "warn",
+        "no-else-return": ["error", { "allowElseIf": false }]
     }
-  }
+}
+
+export const typescript = {
+    rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+
+    }
+}
+
+export default [
+    // sonar.configs.recommended,
+    general,
+    typescript
 ];
